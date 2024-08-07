9d1a4a966503b0557264bfd6c2c6bf86611b6516
Federico Perez <federicoperezramirez@gmail.com>
2024-08-06 22:53:40 -0500

    contact page sending mail

--- contact.js
+++ contact.js
@@ -2,26 +2,17 @@ function emailSend(){
 	var userName = document.getElementById('name').value;
 	var phone = document.getElementById('phone').value;
 	var email = document.getElementById('email').value;
+	var phone = document.getElementById('phone').value;
+	var message = document.getElementById('message').value;
 
-	var messageBody = "Name " + userName +
-	"<br/> Phone " + phone +
-	"<br/> Email " + email;
-	Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "federico.perez@utp.edu.co",	
-    Password : "8D339DE9C51FA4B59699BA3547A3B4692B7A",
-    To : 'federicoperezramirez@gmail.com',
-    From : "federico.perez@utp.edu.co",
-    Subject : "This is the subject",
-    Body : messageBody
-}).then(
-  message => {
-  	if(message=='OK'){
-  		swal("Secussful", "You clicked the button!", "success");
-  	}
-  	else{
-  		swal("Error", "You clicked the button!", "error");
-  	}
-  }
-);
+	emailjs.init({
+		publicKey: "iYHGIDlx4NdEE1Wc_",
+	  });
+	  
+	emailjs.send("service_j5xe5qv","template_9jrh3po",{
+		from_name: userName,
+		email: email,
+		phone: phone,
+		text: message,
+		});
 }
