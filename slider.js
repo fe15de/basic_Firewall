ca6081bd43b9f8d25bead2937976bcba45a0be70
Federico Perez <federicoperezramirez@gmail.com>
2024-09-07 10:40:42 -0500

    Managment Code

--- slider.js
+++ slider.js
@@ -1,12 +1,12 @@
 
-let slider = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let dots = document.querySelectorAll('.slider .dots li');
+var slider = document.querySelector('.slider .list');
+var items = document.querySelectorAll('.slider .list .item');
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+var dots = document.querySelectorAll('.slider .dots li');
 
-let lengthItems = items.length - 1;
-let active = 0;
+var lengthItems = items.length - 1;
+var active = 0;
 next.onclick = function(){
     active = active + 1 <= lengthItems ? active + 1 : 0;
     reloadSlider();
@@ -15,11 +15,11 @@ prev.onclick = function(){
     active = active - 1 >= 0 ? active - 1 : lengthItems;
     reloadSlider();
 }
-let refreshInterval = setInterval(()=> {next.click()}, 3000);
+var refreshInterval = setInterval(()=> {next.click()}, 3000);
 function reloadSlider(){
     slider.style.left = -items[active].offsetLeft + 'px';
     // 
-    let last_active_dot = document.querySelector('.slider .dots li.active');
+    var last_active_dot = document.querySelector('.slider .dots li.active');
     last_active_dot.classList.remove('active');
     dots[active].classList.add('active');
 
